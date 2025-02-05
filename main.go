6c2d5ebdb455297c502c55e47170396e2410c723
nexy7574 <git@nexy7574.co.uk>
2025-02-05 22:25:49 +0000

    add manual backfill commands and fix crash on startup

--- main.go
+++ main.go
@@ -15,17 +15,34 @@ import (
 	"os"
 	"strings"
 	"sync"
+	"time"
 )
 
 var homeserver = flag.String("url", "https://matrix-client.matrix.org", "The URL of the homeserver to connect to")
 var accessToken = flag.String("token", "", "The access token to use for the connection")
 var adminRoomAlias = flag.String("room", "", "The alias of the admin room")
+var resolvedAdminRoomID id.RoomID
 var dryRun = flag.Bool("dry-run", false, "Don't actually send any messages")
 var defederate = flag.Bool("defederate", false, "Ban federation of the room after banning")
+var startupTimestamp = time.Now().UnixMilli()
 
-func sendRoomBan(ctx context.Context, client *mautrix.Client, adminRoom id.RoomID, policyEvent *event.Event) []error {
+func sendRoomBan(ctx context.Context, client *mautrix.Client, policyEvent *event.Event) []error {
+	log.Trace().
+		Stringer("admin_room", resolvedAdminRoomID).
+		Interface("policy_event", policyEvent).
+		Msg("Processing policy event")
+	if strings.HasPrefix(*policyEvent.StateKey, "room") {
+		log.Debug().Str("state_key", *policyEvent.StateKey).Msg("Ignoring legacy room policy event")
+		return nil
+	}
 	sendErrors := make([]error, 0)
-	targetRoomId := policyEvent.Content.Raw["entity"].(string)
+	rawTargetRoomId := policyEvent.Content.Raw["entity"]
+	if rawTargetRoomId == nil {
+		log.Warn().Interface("content", policyEvent.Content).Msg("No entity found in policy event")
+		return nil
+	}
+	targetRoomId := rawTargetRoomId.(string)
+
 	summary, err := client.GetRoomSummary(ctx, targetRoomId)
 	var roomName string
 	if err != nil {
@@ -83,7 +100,7 @@ func sendRoomBan(ctx context.Context, client *mautrix.Client, adminRoom id.RoomI
 		log.Info().Str("room_id", commandParts[len(commandParts)-1]).Msg("Banning room")
 		resp, err := client.SendMessageEvent(
 			ctx,
-			adminRoom,
+			resolvedAdminRoomID,
 			event.EventMessage,
 			body,
 		)
@@ -92,7 +109,7 @@ func sendRoomBan(ctx context.Context, client *mautrix.Client, adminRoom id.RoomI
 			sendErrors = append(sendErrors, err)
 		} else {
 			log.Debug().
-				Stringer("admin_room", adminRoom).
+				Stringer("admin_room", resolvedAdminRoomID).
 				Stringer("event_id", resp.EventID).
 				Msg("Sent message in admin room")
 			lastEvent = resp.EventID
@@ -102,9 +119,166 @@ func sendRoomBan(ctx context.Context, client *mautrix.Client, adminRoom id.RoomI
 	return sendErrors
 }
 
+func handleIncomingCommand(ctx context.Context, db *DBHelper, client *mautrix.Client, evt *event.Event) {
+	message := evt.Content.AsMessage()
+	if message == nil {
+		log.Debug().Interface("event", evt).Msg("Ignoring non-message event")
+		return
+	}
+	if message.MsgType != event.MsgText {
+		log.Debug().Interface("message", message).Msg("Ignoring non-text message")
+		return
+	}
+	args := strings.Split(message.Body, " ")
+	if !strings.HasPrefix(args[0], "?") {
+		log.Debug().Str("command", args[0]).Msg("Ignoring non-admin command")
+		return
+	}
+	command := args[0][1:]
+	switch command {
+	case "ping":
+		body := format.RenderMarkdown("Pong!", true, true)
+		body.MsgType = event.MsgNotice
+		body.RelatesTo = &event.RelatesTo{InReplyTo: &event.InReplyTo{EventID: evt.ID}}
+		_, err := client.SendMessageEvent(
+			ctx,
+			evt.RoomID,
+			event.EventMessage,
+			body,
+		)
+		if err != nil {
+			log.Error().Err(err).Msg("Error sending message")
+		}
+	case "backfill":
+		if len(args) < 2 {
+			log.Debug().Interface("args", args).Msg("Ignoring backfill command with no room ID")
+			return
+		}
+		targetRoomId := id.RoomID(args[1])
+		log.Info().Str("room_id", targetRoomId.String()).Msg("Backfilling events from room")
+		_, err := client.SendNotice(ctx, evt.RoomID, fmt.Sprintf("Backfilling events from room %s", targetRoomId))
+		if err != nil {
+			log.Error().Err(err).Msg("Error sending notice")
+		}
+		// fetch history and manually dispatch
+		nextBatch := ""
+		processedEvents := 0
+		for {
+			events, err := client.Messages(
+				ctx,
+				targetRoomId,
+				nextBatch,
+				"",
+				mautrix.DirectionBackward,
+				&mautrix.FilterPart{
+					Limit: 100,
+					Types: []event.Type{event.StatePolicyRoom},
+				},
+				100,
+			)
+			if err != nil {
+				log.Error().Err(err).Msg("Error fetching events")
+				break
+			}
+			for _, evt2 := range events.Chunk {
+				processPolicyRoomEvent(*db, client)(ctx, evt2)
+				processedEvents++
+			}
+			if events.End == "" {
+				break
+			}
+			nextBatch = events.End
+		}
+		log.Info().Int("count", processedEvents).Msg("Backfilled events")
+		_, err = client.SendNotice(ctx, evt.RoomID, fmt.Sprintf("Backfilled %d events from room %s", processedEvents, targetRoomId))
+		if err != nil {
+			log.Error().Err(err).Msg("Error sending notice")
+		}
+	case "help":
+		body := format.RenderMarkdown(
+			"Available commands:\n"+
+				" - `?ping`: Responds with 'Pong!'\n"+
+				" - `?backfill <room_id>`: Backfills events from the specified room\n"+
+				" - `?help`: Shows this help message",
+			true,
+			true,
+		)
+		body.MsgType = event.MsgNotice
+		body.RelatesTo = &event.RelatesTo{InReplyTo: &event.InReplyTo{EventID: evt.ID}}
+		_, err := client.SendMessageEvent(
+			ctx,
+			evt.RoomID,
+			event.EventMessage,
+			body,
+		)
+		if err != nil {
+			log.Error().Err(err).Msg("Error sending message")
+		}
+	default:
+		log.Debug().Str("command", command).Msg("Ignoring unknown command")
+	}
+}
+
+func processMessageEvent(bansDB DBHelper, client *mautrix.Client) func(ctx context.Context, event *event.Event) {
+	return func(ctx context.Context, event *event.Event) {
+		if event.Timestamp < startupTimestamp {
+			log.Debug().Interface("event", event).Msg("Ignoring old event")
+			return
+		}
+		if event.RoomID.String() == resolvedAdminRoomID.String() {
+			handleIncomingCommand(ctx, &bansDB, client, event)
+		}
+	}
+}
+
+func processPolicyRoomEvent(bansDB DBHelper, client *mautrix.Client) func(ctx context.Context, evt *event.Event) {
+	return func(ctx context.Context, evt *event.Event) {
+		if evt.Type != event.StatePolicyRoom {
+			log.Debug().Interface("event", evt).Msg("Ignoring non-room-policy room event")
+			return
+		}
+		exists, err := bansDB.GetBan(evt.RoomID.String())
+		if exists.ID.String() != "" {
+			log.Trace().Str("room", evt.RoomID.String()).Msg("Ban already exists for room.")
+			return
+		}
+		if err != nil {
+			// If the error is just "not found", ignore.
+			if !errors.Is(err, sql.ErrNoRows) {
+				log.Error().Err(err).Msg("Error getting ban from DB")
+			} else {
+				log.Debug().Str("room", evt.RoomID.String()).Msg("No ban found in DB")
+			}
+		} else {
+			log.Debug().Str("room", evt.RoomID.String()).Msg("Ban found in DB")
+			return
+		}
+		log.Info().
+			Str("room", evt.RoomID.String()).
+			Interface("content", evt).
+			Msg("Got policy rule evt")
+		sendErrors := sendRoomBan(ctx, client, evt)
+		if len(sendErrors) > 0 {
+			log.Error().Errs("errors", sendErrors).Msg("Errors sending messages")
+		}
+		err = bansDB.AddBan(evt.RoomID.String(), *evt)
+		if err != nil {
+			log.Error().Err(err).Msg("Error adding ban to DB")
+		}
+	}
+}
+
 func main() {
 	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
-	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
+	logLevel := os.Getenv("LOG_LEVEL")
+	if logLevel == "" {
+		logLevel = "info"
+	}
+	logLevelParsed, err := zerolog.ParseLevel(logLevel)
+	if err != nil {
+		panic(err)
+	}
+	log.Logger = log.Level(logLevelParsed).Output(zerolog.ConsoleWriter{Out: os.Stderr})
 	flag.Parse()
 	ctx := context.Background()
 	log.Trace().
@@ -143,40 +317,15 @@ func main() {
 	} else if !strings.HasPrefix(*adminRoomAlias, "!") {
 		panic("Invalid room ID or alias")
 	}
-	roomIdObj := id.RoomID(roomId)
+	resolvedAdminRoomID = id.RoomID(roomId)
 
 	syncer := client.Syncer.(*mautrix.DefaultSyncer)
 	bansDB, err := NewDBHelper()
 	if err != nil {
 		panic(err)
 	}
-	syncer.OnEventType(event.StatePolicyRoom, func(ctx context.Context, event *event.Event) {
-		exists, err := bansDB.GetBan(event.RoomID.String())
-		if exists.ID.String() != "" {
-			log.Trace().Str("room", event.RoomID.String()).Msg("Ban already exists for room.")
-			return
-		}
-		if err != nil {
-			// If the error is just "not found", ignore.
-			if !errors.Is(err, sql.ErrNoRows) {
-				log.Error().Err(err).Msg("Error getting ban from DB")
-			} else {
-				log.Debug().Str("room", event.RoomID.String()).Msg("No ban found in DB")
-			}
-		}
-		log.Info().
-			Str("room", event.RoomID.String()).
-			Interface("content", event).
-			Msg("Got policy rule event")
-		sendErrors := sendRoomBan(ctx, client, roomIdObj, event)
-		if len(sendErrors) > 0 {
-			log.Error().Errs("errors", sendErrors).Msg("Errors sending messages")
-		}
-		err = bansDB.AddBan(event.RoomID.String(), *event)
-		if err != nil {
-			log.Error().Err(err).Msg("Error adding ban to DB")
-		}
-	})
+	syncer.OnEventType(event.StatePolicyRoom, processPolicyRoomEvent(*bansDB, client))
+	syncer.OnEventType(event.EventMessage, processMessageEvent(*bansDB, client))
 	// Start syncing
 	syncCtx, cancelSync := context.WithCancel(ctx)
 	var syncStopWait sync.WaitGroup
